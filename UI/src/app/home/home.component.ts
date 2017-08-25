54578fd94e9a8b8e5c7c313ea6534f14de328497
SelinaXM <celina0330@hotmail.com>
2017-08-25 11:36:51 -0400

    updating the button

--- UI/src/app/home/home.component.ts
+++ UI/src/app/home/home.component.ts
@@ -138,7 +138,7 @@ export class HomeComponent implements OnInit {
 
                   }
 
-                  content.innerHTML += '<tr><td align="left"><button id="undercontract" onclick="sendMsg(\'' + self.users[id].street + '\')">Under Contract</button></td></tr>';
+                  '<tr><td><button id="undercontract" onclick="sendMsg(\'' + self.users[id].long + ":" + self.users[id].lat + ":" + self.users[id].id + '\')">Under Contract</button></td></tr>';
                   content.innerHTML += '</table>' +
                     '</div>' ;
 
